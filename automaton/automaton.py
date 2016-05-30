4fdc0f341545ed2050542eb29efa5392e40c7f17
Christopher McCormack <mccormack.christopher@gmail.com>
2016-05-29 21:23:32 -0700

    Added Automoton kata

---
+++ automaton/automaton.py
@@ -0,0 +1,11 @@
+class Automaton(object):
+
+    def __init__(self):
+        self.states = []
+
+    def read_commands(self, commands):
+        # Return True if we end in our accept state, False otherwise
+
+my_automaton = Automaton()
+
+# Do anything necessary to set up your automaton's states, q1, q2, and q3.
